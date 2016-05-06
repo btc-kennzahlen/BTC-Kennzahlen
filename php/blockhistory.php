6dca8c02c3779c0ced72c5900d3c14bf8e903993
btc-kennzahlen <btc.kennzahlen@gmail.com>
2016-05-06 15:18:39 +0200

    Update blockhistory.php

--- php/blockhistory.php
+++ php/blockhistory.php
@@ -5,12 +5,12 @@
 
 
 //Start Werte für das Live-Net
-//$hash = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"; // Blockhash von dem ersten Block 
-//$ts = 1231006505; // Zeitpunkt des ersten Blocks
+$hash = "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"; // Blockhash von dem ersten Block 
+$ts = 1231006505; // Zeitpunkt des ersten Blocks
 
 // Start Werte für das Test-Net
-$hash = "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"; // Blockhash von dem ersten Block
-$ts = 1296688602; // Zeitpunkt des ersten Blocks
+//$hash = "000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"; // Blockhash von dem ersten Block
+//$ts = 1296688602; // Zeitpunkt des ersten Blocks
 
 
 $trx = 0; 
